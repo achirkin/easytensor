be7718ad97dec2e1d87516a3557ce98aa50d0fb6
achirkin <achirkin@users.noreply.github.com>
2016-11-26 12:37:24 +0100

    More mindblowing

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE KindSignatures, DataKinds #-}
-{-# LANGUAGE TypeOperators, FlexibleInstances, ScopedTypeVariables #-}
 module Main where
 
 import Data.Tensor
 
 main :: IO ()
-main = putStrLn $ show (Tensor :: Tensor Double '[1,4,2] '[2,4,2])
+main = do
+  putStrLn $ show (vec2 1 2 `plus` vec2 3 4)
+  print (1 .< 4 .< two)
+  where
+    two = vec2 2 2
