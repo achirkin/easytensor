032383bd15469fc4ea07beb107dd8ab17a1f4c0d
achirkin <chirkin@arch.ethz.ch>
2017-04-26 16:47:41 +0200

    Again got problems because of Snoc op...

---
+++ initws.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export PATH=/usr/local/ghc-8.2.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/binexport PATH=/usr/local/ghc-8.2.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+rm -rf dist
+rm -rf dist-newstyle
+cabal update
+cabal sandbox init
+pushd .cabal-sandbox
+git clone --recursive https://github.com/achirkin/ghc-mod.git
+popd
+cabal sandbox add-source .cabal-sandbox/ghc-mod
+cabal sandbox add-source .cabal-sandbox/ghc-mod/vendor/ghc-syb/utils
+cabal sandbox add-source .cabal-sandbox/ghc-mod/vendor/cabal-helper
+cabal install ghc-mod QuickCheck --allow-newer --ghc-options="-XTypeSynonymInstances"
+cabal configure
