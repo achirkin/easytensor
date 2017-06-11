30e3f278ffdb9b23ad74e72623c73cc4c2a050b3
achirkin <achirkin@users.noreply.github.com>
2017-06-11 14:02:08 +0200

    Restored all remaining tests added more inference functions.

--- easytensor/test/Numeric/DataFrame/SubSpaceTest.hs
+++ easytensor/test/Numeric/DataFrame/SubSpaceTest.hs
@@ -17,7 +17,6 @@
 module Numeric.DataFrame.SubSpaceTest (runTests) where
 
 import           Test.QuickCheck
-import           Data.Type.Equality
 import           Numeric.DataFrame.Arbitraries
 import           Data.Proxy
 
@@ -37,24 +36,38 @@ prop_Dims (SSDF (SDF (x :: DataFrame Float xs))) (SSDF (SDF (y :: DataFrame Floa
 prop_Eye :: SomeSimpleDFNonScalar -> Bool
 prop_Eye (SSDFN (SDF (x :: DataFrame Float (d ': ds))))
   | DimensionsEvidence <- inferTailDimensions x
-  = case ( unsafeEqProof :: Prefix ds (d ': ds) :~: '[d]
-         , unsafeEqProof :: IsSuffix ds (d ': ds) :~: 'True
-         , unsafeEqProof :: Suffix '[d] (d ': ds) :~: ds
-         , unsafeEqProof :: Concat '[d] ds :~: d ': ds
-         ) of
-    (Refl, Refl, Refl, Refl) -> eye %* x == x
+  , DimensionsEvidence <- inferInitDimensions x
+  , NonEmptyListEvidence <- inferNonEmptyList @d @ds
+  , ValidDim <- inferLastValidDim @d @ds
+  = eye %* x == x && x == x %* eye
 
 
-prop_VariousFixed :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
-prop_VariousFixed (SDF x) (SDF y) = and
-   [ ((dimMax `inSpaceOf` y) !. z) == x
-   , (1:!3 !. z) == x
-   , (2:!2 !. z) %* eye == x
-   , ewfoldl y (+) 10 z == ewfoldr y (+) 0 z + 10
-   , y * 2 == ewmap (dim @'[7]) (*2) y
-   ]
+prop_IndexDimMax :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
+prop_IndexDimMax (SDF x) (SDF y) =
+   ((dimMax `inSpaceOf` y) !. z) == x
   where
     z = ewgen y x :: DataFrame Float '[2,5,4,3,7]
+
+prop_IndexCustom1 :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
+prop_IndexCustom1 (SDF x) (SDF y) = (1:!3 !. z) == x
+  where
+    z = ewgen y x :: DataFrame Float '[2,5,4,3,7]
+
+prop_IndexCustom2 :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
+prop_IndexCustom2 (SDF x) (SDF y) = (2:!2 !. z) %* eye == x
+  where
+    z = ewgen y x :: DataFrame Float '[2,5,4,3,7]
+
+prop_Foldlr :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
+prop_Foldlr (SDF x) (SDF y) =
+   abs (ewfoldl y (+) 10 z - ewfoldr y (+) 0 z - 10) < ewgen x (zmax * 0.0001)
+  where
+    z = ewgen y x :: DataFrame Float '[2,5,4,3,7]
+    zmax = ewfoldl z (max . abs) 0 z
+
+prop_Ewmap :: SimpleDF '[2,5,4] -> SimpleDF '[3,7] -> Bool
+prop_Ewmap (SDF _) (SDF y) =
+   y * 2 == ewmap (dim @'[7]) (*2) y
 
 
 return []
