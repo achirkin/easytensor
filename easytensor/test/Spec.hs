c7051c2cd9e76712bb412255777f649a2c98d9ff
achirkin <chirkin@arch.ethz.ch>
2017-06-14 15:04:40 +0200

    Changed tests type

--- easytensor/test/Spec.hs
+++ easytensor/test/Spec.hs
@@ -1,14 +1,26 @@
-import           Control.Monad                  (unless)
+module Spec (tests) where
+
+import           Distribution.TestSuite
 
 import qualified Numeric.DataFrame.BasicTest
 import qualified Numeric.DataFrame.SubSpaceTest
 
-main :: IO ()
-main = do
-  test "DataFrame.Basic"    Numeric.DataFrame.BasicTest.runTests
-  test "DataFrame.SubSpace" Numeric.DataFrame.SubSpaceTest.runTests
+tests :: IO [Test]
+tests = return
+  [ test "DataFrame.Basic"    Numeric.DataFrame.BasicTest.runTests
+  , test "DataFrame.SubSpace" Numeric.DataFrame.SubSpaceTest.runTests
+  ]
 
 
-
-test :: String -> IO Bool -> IO ()
-test name t = t >>= flip unless (putStrLn $ name ++ ": failure!")
+test :: String -> IO Bool -> Test
+test tName propOp = Test testI
+  where
+    testI = TestInstance
+        { run = fromBool <$> propOp
+        , name = tName
+        , tags = []
+        , options = []
+        , setOption = \_ _ -> Right testI
+        }
+    fromBool False = Finished (Fail "Property does not hold!")
+    fromBool True  = Finished Pass
