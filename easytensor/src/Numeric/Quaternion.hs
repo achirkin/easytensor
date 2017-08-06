85214b6a07bdbda1d7ef2fcc9476dbc81ce14d8c
Artem Chirkin <chirkin@arch.ethz.ch>
2017-08-06 15:25:12 +0200

    JS implementation of QDouble

---
+++ easytensor/src/Numeric/Quaternion.hs
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Numeric.Quaternion
+-- Copyright   :  (c) Artem Chirkin
+-- License     :  BSD3
+--
+-- Maintainer  :  chirkin@arch.ethz.ch
+--
+--
+-----------------------------------------------------------------------------
+module Numeric.Quaternion
+    ( module Q
+    ) where
+
+import Numeric.Quaternion.Class as Q
+-- import Numeric.Quaternion.QFloat ()
+import Numeric.Quaternion.QDouble ()
