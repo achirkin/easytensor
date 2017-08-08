9268aaf45cc34c0682cbc142130620f054609f3f
achirkin <achirkin@users.noreply.github.com>
2017-08-08 08:08:44 +0200

    Switched dimensions to exitcode tests

--- dimensions/test/Spec.hs
+++ dimensions/test/Spec.hs
@@ -1,15 +1,35 @@
-module Spec (tests) where
+module Main (tests, main) where
 
 import           Distribution.TestSuite
 
 import qualified Numeric.Dimensions.ListTest
 
+
+-- | Collection of tests in detailed-0.9 format
 tests :: IO [Test]
 tests = return
   [ test "Dimensions.List"    Numeric.Dimensions.ListTest.runTests
   ]
 
 
+
+
+-- | Run tests as exitcode-stdio-1.0
+main :: IO Int
+main = do
+    ts <- tests
+    trs <- mapM (\(Test ti) ->(,) (name ti) <$> run ti) ts
+    case filter (not . isGood) trs of
+       [] -> return 0
+       xs -> do
+        putStrLn $ "Failed tests: " ++ unwords (fmap fst xs)
+        return 1
+  where
+    isGood (_, Finished Pass) = True
+    isGood _ = False
+
+
+-- | Convert QuickCheck props into Cabal tests
 test :: String -> IO Bool -> Test
 test tName propOp = Test testI
   where
