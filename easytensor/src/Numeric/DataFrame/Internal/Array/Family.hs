1e5047e4e85bb75b4e7a2403a8d107117c24da3e
Artem M. Chirkin <chirkin@arch.ethz.ch>
2018-05-02 12:41:09 +0200

    Added common class witnesses

--- easytensor/src/Numeric/DataFrame/Internal/Array/Family.hs
+++ easytensor/src/Numeric/DataFrame/Internal/Array/Family.hs
@@ -12,6 +12,7 @@
 {-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE TypeInType             #-}
+{-# LANGUAGE TypeApplications       #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Numeric.DataFrame.Internal.Array.Family
@@ -26,38 +27,17 @@
 module Numeric.DataFrame.Internal.Array.Family
   ( Array, Scalar (..), ArrayBase (..)
   , ArraySingleton (..)
-  --  Array
-  -- , ArrayF (..), ArrayD (..)
-  -- , ArrayI (..), ArrayI8 (..), ArrayI16 (..), ArrayI32 (..), ArrayI64 (..)
-  -- , ArrayW (..), ArrayW8 (..), ArrayW16 (..), ArrayW32 (..), ArrayW64 (..)
-  -- , Scalar (..)
-  -- , FloatX2 (..), FloatX3 (..), FloatX4 (..)
-  -- , DoubleX2 (..), DoubleX3 (..), DoubleX4 (..)
-  -- , ArrayInstanceInference, ElemType (..), ArraySize (..)
-  -- , ElemTypeInference (..), ArraySizeInference (..), ArrayInstanceEvidence
-  -- , getArrayInstance, ArrayInstance (..), inferArrayInstance
+  , ArraySing (..), aSingEv
+  , witnessPrim, witnessEq, witnessShow, witnessOrd, witnessNum, witnessFractional, witnessFloating
   ) where
 
 
--- import           Data.Proxy
--- import           Data.Int                  (Int16, Int32, Int64, Int8)
--- import           Data.Type.Equality        ((:~:) (..))
--- import           Data.Word                 (Word16, Word32, Word64, Word8)
 import           GHC.Base
--- (ByteArray#, Double#, Float#, Int#
-                                           --,Word#, unsafeCoerce#
--- #if WORD_SIZE_IN_BITS < 64
---                                            ,Int64#, Word64#
--- #endif
-                                        --   )
 
--- import           Numeric.DataFrame.Internal.Array.ElementWise
--- import           Numeric.Commons
--- import           Numeric.DataFrame.Internal.Array.Class
--- import           Numeric.DataFrame.Internal.Array.Internal
--- import           Numeric.Dimensions
--- import           Numeric.PrimBytes
 
+import           Numeric.DataFrame.Internal.Array.Class
+import           Numeric.Dimensions
+import           Numeric.PrimBytes
 import           Numeric.DataFrame.Internal.Array.Family.ArrayBase
 import           Numeric.DataFrame.Internal.Array.Family.DoubleX2
 import           Numeric.DataFrame.Internal.Array.Family.DoubleX3
@@ -93,40 +73,46 @@ class ArraySingleton (t :: Type) (ds :: [Nat]) where
     aSing :: ArraySing t ds
 
 data ArraySing t (ds :: [Nat]) where
-    AScalar :: (Array t ds ~ Scalar t)       => ArraySing t     '[]
-    AF2     :: (Array t ds ~ FloatX2)        => ArraySing Float  ds
-    AF3     :: (Array t ds ~ FloatX3)        => ArraySing Float  ds
-    AF4     :: (Array t ds ~ FloatX4)        => ArraySing Float  ds
-    AD2     :: (Array t ds ~ DoubleX2)       => ArraySing Double ds
-    AD3     :: (Array t ds ~ DoubleX3)       => ArraySing Double ds
-    AD4     :: (Array t ds ~ DoubleX4)       => ArraySing Double ds
-    ABase   :: (Array t ds ~ ArrayBase t ds) => ArraySing t      ds
+    AScalar :: (Array t ds ~ Scalar t)       => ArraySing t      '[]
+    AF2     :: (Array t ds ~ FloatX2)        => ArraySing Float  '[2]
+    AF3     :: (Array t ds ~ FloatX3)        => ArraySing Float  '[3]
+    AF4     :: (Array t ds ~ FloatX4)        => ArraySing Float  '[4]
+    AD2     :: (Array t ds ~ DoubleX2)       => ArraySing Double '[2]
+    AD3     :: (Array t ds ~ DoubleX3)       => ArraySing Double '[3]
+    AD4     :: (Array t ds ~ DoubleX4)       => ArraySing Double '[4]
+    ABase   :: ( Array t ds ~ ArrayBase t ds
+               , PrimBytes t
+               ) => ArraySing t ds
 
 deriving instance Eq (ArraySing t ds)
 deriving instance Ord (ArraySing t ds)
 deriving instance Show (ArraySing t ds)
 
 
---
--- -- | This function does GHC's magic to convert user-supplied `aSing` function
--- --   to create an instance of `ArraySingleton` typeclass at runtime.
--- --   The trick is taken from Edward Kmett's reflection library explained
--- --   in https://www.schoolofhaskell.com/user/thoughtpolice/using-reflection
--- reifyArraySing :: forall r k t ds
---                 . ArraySing k t ds -> ( ArraySingleton k t ds => r) -> r
--- reifyArraySing as k
---   = unsafeCoerce# (MagicArraySing k :: MagicArraySing k t ds r) as
--- {-# INLINE reifyArraySing #-}
--- newtype MagicArraySing k t (ds :: [k]) r
---   = MagicArraySing (ArraySingleton k t ds => r)
---
--- aSingEv :: ArraySing k t ds -> Evidence (ArraySingleton k t ds)
--- aSingEv ds = reifyArraySing ds E
--- {-# INLINE aSingEv #-}
+
+-- | This function does GHC's magic to convert user-supplied `aSing` function
+--   to create an instance of `ArraySingleton` typeclass at runtime.
+--   The trick is taken from Edward Kmett's reflection library explained
+--   in https://www.schoolofhaskell.com/user/thoughtpolice/using-reflection
+reifyArraySing :: forall r t ds
+                . ArraySing t ds -> ( ArraySingleton t ds => r) -> r
+reifyArraySing as k
+  = unsafeCoerce# (MagicArraySing k :: MagicArraySing t ds r) as
+{-# INLINE reifyArraySing #-}
+newtype MagicArraySing t (ds :: [Nat]) r
+  = MagicArraySing (ArraySingleton t ds => r)
+
+-- | Use `ArraySing` GADT to construct an `ArraySingleton` dictionary.
+--   In other words, bring an evidence of `ArraySingleton` instance into
+--   a scope at runtime.
+aSingEv :: ArraySing t ds -> Evidence (ArraySingleton t ds)
+aSingEv ds = reifyArraySing ds E
+{-# INLINE aSingEv #-}
 
 
 
-instance {-# OVERLAPPABLE #-} ArraySingleton t ds where
+instance {-# OVERLAPPABLE #-}
+         PrimBytes t => ArraySingleton t ds where
     aSing = unsafeCoerce# (ABase :: ArraySing t '[0])
 instance {-# OVERLAPPING #-}  ArraySingleton t      '[]    where
     aSing = AScalar
@@ -142,3 +128,98 @@ instance {-# OVERLAPPING #-}  ArraySingleton Double '[3]   where
     aSing = AD3
 instance {-# OVERLAPPING #-}  ArraySingleton Double '[4]   where
     aSing = AD4
+
+witnessPrim :: forall t ds
+             . ( PrimBytes t
+               , ArraySingleton t ds
+               , Dimensions ds
+               )
+            => Evidence (PrimBytes (Array t ds), PrimArray t (Array t ds))
+witnessPrim = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessEq :: forall t ds
+           . (Eq t, ArraySingleton t ds)
+          => Evidence (Eq (Array t ds))
+witnessEq = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessOrd :: forall t ds
+            . (Ord t, ArraySingleton t ds)
+           => Evidence (Ord (Array t ds))
+witnessOrd = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessNum :: forall t ds
+            . (Num t, ArraySingleton t ds)
+           => Evidence (Num (Array t ds))
+witnessNum = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessFractional :: forall t ds
+                   . (Fractional t, ArraySingleton t ds)
+                  => Evidence (Fractional (Array t ds))
+witnessFractional = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessFloating :: forall t ds
+                 . (Floating t, ArraySingleton t ds)
+                => Evidence (Floating (Array t ds))
+witnessFloating = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
+witnessShow :: forall t ds
+           . (Show t, Dimensions ds, ArraySingleton t ds)
+          => Evidence (Show (Array t ds))
+witnessShow = case (aSing :: ArraySing t ds) of
+  AScalar -> E
+  AF2 -> E
+  AF3 -> E
+  AF4 -> E
+  AD2 -> E
+  AD3 -> E
+  AD4 -> E
+  ABase -> E
+
