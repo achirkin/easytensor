48289da53c1c8134e5128b19c7c86e2c08f42d99
achirkin <achirkin@users.noreply.github.com>
2019-07-31 10:56:30 +0200

    Added XxxList classes for better type list ops inference

--- dimensions/src/Data/Type/List.hs
+++ dimensions/src/Data/Type/List.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE ConstraintKinds        #-}
-{-# LANGUAGE DataKinds              #-}
-{-# LANGUAGE FlexibleContexts       #-}
-{-# LANGUAGE FlexibleInstances      #-}
-{-# LANGUAGE GADTs                  #-}
-{-# LANGUAGE PolyKinds              #-}
-{-# LANGUAGE ScopedTypeVariables    #-}
-{-# LANGUAGE TypeApplications       #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE TypeInType             #-}
-{-# LANGUAGE TypeOperators          #-}
-{-# LANGUAGE UndecidableInstances   #-}
+{-# LANGUAGE ConstraintKinds      #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE ExplicitNamespaces   #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE PolyKinds            #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeInType           #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Type.List
@@ -30,18 +30,18 @@ module Data.Type.List
   , Reverse, Take, Drop, Length
     -- * Operations on elements
   , All, Map, Elem
-    -- * Concatenation and its evidence
-  , ConcatList, evStripSuffix, evStripPrefix, evConcat
+    -- * Classes that simplify inference of type equalities
+  , SnocList, ReverseList, ConcatList
+  , inferStripSuffix, inferStripPrefix, inferConcat
     -- * Data.Typeable
   , inferTypeableCons
   ) where
 
-import Data.Constraint         ((:-) (..), Constraint, Dict (..))
-import Data.Type.List.Internal (Snoc)
+import Data.Constraint         (Constraint, Dict (..))
+import Data.Type.List.Internal
 import Data.Type.Lits
 import GHC.Base                (Type)
 import Type.Reflection
-import Unsafe.Coerce           (unsafeCoerce)
 
 -- | Empty list, same as @'[]@.
 type Empty = '[]
@@ -52,25 +52,11 @@ type Cons (a :: k) (as :: [k])
 
 -- | Extract the first element of a list, which must be non-empty.
 type family Head (xs :: [k]) :: k where
-    Head ('[] :: [k]) = TypeError ( ListError k "Head: empty type-level list." )
     Head (x ': _)     = x
 
 -- | Extract the elements after the head of a list, which must be non-empty.
 type family Tail (xs :: [k]) :: [k] where
-    Tail ('[] :: [k]) = TypeError ( ListError k "Tail: empty type-level list." )
     Tail (_ ': xs)    = xs
-
--- | Extract the last element of a list, which must be non-empty.
-type family Last (xs :: [k]) :: k where
-    Last ('[] :: [k]) = TypeError ( ListError k "Last: empty type-level list." )
-    Last '[x]         = x
-    Last (_ ': xs)    = Last xs
-
--- | Extract all but last elements of a list, which must be non-empty.
-type family Init (xs :: [k]) :: [k] where
-    Init ('[] :: [k]) = TypeError ( ListError k "Init: empty type-level list." )
-    Init '[x]         = '[]
-    Init (x ': xs)    = x ': Init xs
 
 -- | @Take n xs@ returns the prefix of a list of length @max n (length xs)@.
 type family Take (n :: Nat) (xs :: [k]) :: [k] where
@@ -83,64 +69,6 @@ type family Drop (n :: Nat) (xs :: [k]) :: [k] where
     Drop 0  xs       = xs
     Drop n (_ ': xs) = Drop (n - 1) xs
     Drop _ '[]       = '[]
-
--- | Append two lists.
-type family Concat (as :: [k]) (bs :: [k]) :: [k] where
-    Concat '[]        bs       = bs
-    Concat  as       '[]       = as
-    Concat (a ': as)  bs       = a ': Concat as bs
-
--- | Remove prefix @as@ from a list @asbs@ if @as@ is a prefix; fail otherwise.
-type family StripPrefix (as :: [k]) (asbs :: [k]) :: [k] where
-    StripPrefix  as        as         = '[]
-    StripPrefix '[]        asbs       = asbs
-    StripPrefix (a ': as) (a ': asbs) = StripPrefix as asbs
-    StripPrefix (a ': as) (b ': asbs)
-      = TypeError
-        ( 'Text "StripPrefix: the first argument is not a prefix of the second."
-        ':$$:
-          'Text "Failed prefix: " ':<>: 'ShowType (a ': as)
-        ':$$:
-          'Text "Second argument: " ':<>: 'ShowType (b ': asbs)
-        )
-    StripPrefix (a ': as) '[]
-      = TypeError
-        ( 'Text "StripPrefix: the first argument is longer than the second."
-        ':$$:
-          'Text "Failed prefix: " ':<>: 'ShowType (a ': as)
-        ':$$:
-          'Text "The reduced second argument is empty."
-        )
-
--- | Remove suffix @bs@ from a list @asbs@ if @bs@ is a suffix; fail otherwise.
-type family StripSuffix (bs :: [k]) (asbs :: [k]) :: [k] where
-    StripSuffix  bs        bs         = '[]
-    StripSuffix '[]        asbs       = asbs
-    StripSuffix (a ': bs) (b ': bs)
-      = TypeError
-        ( 'Text "StripSuffix: the first argument is not a suffix of the second."
-        ':$$:
-          'Text "Failed match: "
-            ':<>: 'ShowType ((a ': bs) ~ (b ': bs))
-        )
-    StripSuffix (b ': bs) '[]
-      = TypeError
-        ( 'Text "StripSuffix: the first argument is longer than the second."
-        ':$$:
-          'Text "Failed suffix: " ':<>: 'ShowType (b ': bs)
-        ':$$:
-          'Text "The reduced second argument is empty."
-        )
-    StripSuffix  bs       (a ': asbs) = a ': StripSuffix bs asbs
-
--- | Returns the elements of a list in reverse order.
-type family Reverse (xs :: [k]) :: [k] where
-    -- Note: the goal is not to write a fast implementation,
-    --       but make it easier for the type checker to simplify things.
-    --       This is only going to be executed during compile time,
-    --       so there is no real performance impact.
-    Reverse '[] = '[]
-    Reverse (x ': xs) = Snoc (Reverse xs) x
 
 -- | Number of elements in a list.
 type family Length (xs :: [k]) :: Nat where
@@ -173,52 +101,6 @@ type family Map (f :: a -> b) (xs :: [a]) :: [b] where
 type family Elem (x :: k) (xs :: [k]) :: Constraint where
     Elem x (x ': xs) = ()
     Elem x (_ ': xs) = Elem x xs
-
-type ListError k t
-    = 'Text t ':$$:
-    ( 'Text "Type-level error occured when operating on a list of kind "
-      ':<>: 'ShowType [k] ':<>: 'Text "."
-    )
-
--- | Represent a triple of lists forming a relation @(as ++ bs) ~ asbs@
-type ConcatList (as :: [k]) (bs :: [k]) (asbs :: [k]) =
-    ( asbs ~ Concat as bs
-    , as   ~ StripSuffix bs asbs
-    , bs   ~ StripPrefix as asbs
-    )
-
--- | Derive @ConcatList@ given @Concat@
-evConcat :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
-          . (asbs ~ Concat as bs) :- ConcatList as bs asbs
-evConcat = Sub $ unsafeCoerce
-  ( Dict :: Dict
-    ( asbs ~ Concat as bs
-    , as   ~ as
-    , bs   ~ bs
-    )
-  )
-
--- | Derive @ConcatList@ given @StripSuffix@
-evStripSuffix :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
-               . (as ~ StripSuffix bs asbs) :- ConcatList as bs asbs
-evStripSuffix = Sub $ unsafeCoerce
-  ( Dict :: Dict
-    ( asbs ~ asbs
-    , as   ~ StripSuffix bs asbs
-    , bs   ~ bs
-    )
-  )
-
--- | Derive @ConcatList@ given @StripPrefix@
-evStripPrefix :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
-               . (bs ~ StripPrefix as asbs) :- ConcatList as bs asbs
-evStripPrefix = Sub $ unsafeCoerce
-  ( Dict :: Dict
-    ( asbs ~ asbs
-    , as   ~ as
-    , bs   ~ StripPrefix as asbs
-    )
-  )
 
 -- | Given a @Typeable@ list, infer this constraint for its parts.
 inferTypeableCons :: forall (k :: Type) (ys :: [k]) (x :: k) (xs :: [k])
--- dimensions/src/Data/Type/List/Internal.hs
+++ dimensions/src/Data/Type/List/Internal.hs
@@ -1,27 +1,232 @@
-{-# OPTIONS_GHC -fobject-code       #-}
-{-# OPTIONS_GHC -fplugin Data.Type.List.InjectiveSnoc #-}
 {-# OPTIONS_HADDOCK hide, prune     #-}
-{-# LANGUAGE DataKinds              #-}
-{-# LANGUAGE PolyKinds              #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE DataKinds               #-}
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE FunctionalDependencies  #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE PolyKinds               #-}
+{-# LANGUAGE ScopedTypeVariables     #-}
+{-# LANGUAGE TypeApplications        #-}
+{-# LANGUAGE TypeFamilyDependencies  #-}
+{-# LANGUAGE TypeOperators           #-}
+{-# LANGUAGE UndecidableInstances    #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# OPTIONS_GHC -fplugin Data.Constraint.Deriving #-}
+module Data.Type.List.Internal
+  ( Snoc, Init, Last, Reverse
+  , StripSuffix, StripPrefix, Concat
+    -- * Classes that simplify inference of type equalities
+  , SnocList, ReverseList, ConcatList
+  , inferStripSuffix, inferStripPrefix, inferConcat
+    -- auto-generating instances
+  , consInstSnocList, consInstReverseList, consInstConcatList
+  , incohInstReverseList
+  ) where
 
-{-# LANGUAGE TypeInType             #-}
-{-# LANGUAGE UndecidableInstances   #-}
---------------------------------------------------------------------------------
--- |
--- Module      :  Data.Type.List.Internal
--- Copyright   :  (c) Artem Chirkin
--- License     :  BSD3
---
--- Thanks to @Data.Type.List.InjectiveSnoc@, @Snoc@ appears to be injective
--- for an oustide viewer.
---
---
---------------------------------------------------------------------------------
-module Data.Type.List.Internal ( Snoc ) where
+import Data.Constraint          (Dict (..))
+import Data.Constraint.Deriving
+import GHC.Base                 (Type)
+import Unsafe.Coerce            (unsafeCoerce)
 
--- | Appending a list on the other side.
-type family Snoc (xs :: [k]) (x :: k) = (ys :: [k]) where
-    Snoc (x ': xs) y = x ': Snoc xs y
-    Snoc '[]       y = '[y]
+-- | Extract the last element of a list, which must be non-empty.
+type family Last (xs :: [k]) :: k where
+    Last '[x]         = x
+    Last (_ ': xs)    = Last xs
+
+-- | Extract all but last elements of a list, which must be non-empty.
+type family Init (xs :: [k]) :: [k] where
+    Init '[x]         = '[]
+    Init (x ': xs)    = x ': Init xs
+
+-- | Appending a list on the other side (injective).
+type Snoc (xs :: [k]) (x :: k) = RunList (Snoc' xs x)
+
+-- | Reverse elements of a list (injective).
+type Reverse (xs :: [k]) = RunList (Reverse' xs)
+
+-- | A helper data type that makes possible injective `Snoc` and `Reverse` families.
+--
+--   It assures GHC type checker that the `Snoc` operation on a non-empty list
+--   yields a list that contains at least two elements.
+data List k
+  = Empty
+  | Single k
+  | TwoOrMore [k]
+    -- ^ An important invariant: the argument list contains at least two elements.
+
+type family RunList (xs :: List k) = (ys :: [k]) | ys -> k xs where
+    RunList  'Empty                     = '[]
+    RunList ('Single x)                 = '[x]
+    RunList ('TwoOrMore (x ': y ': xs)) = x ': y ': xs
+
+type family Snoc' (xs :: [k]) (x :: k) = (ys :: List k) | ys -> k xs x where
+    Snoc' '[]       y = 'Single y
+    Snoc' (x ': xs) y = 'TwoOrMore (x ': RunList (Snoc' xs y))
+
+type family Reverse' (xs :: [k]) = (ys :: List k) | ys -> k xs where
+    Reverse' '[]            = 'Empty
+    Reverse' '[x]           = 'Single x
+    Reverse' (y ': x ': xs) = 'TwoOrMore (Snoc (RunList (Reverse' (x ': xs))) y)
+
+
+
+-- | Represent a decomposition of a list by appending an element to its end.
+class
+    (bs ~ Snoc as a, as ~ Init bs, a ~ Last bs)
+      => SnocList (as :: [k]) (a :: k) (bs :: [k])
+            | as a -> bs, bs -> as a, as -> k, a -> a, bs -> k
+
+{-# ANN defineSnocList ClassDict #-}
+defineSnocList :: forall (k :: Type) (as :: [k]) (a :: k) (bs :: [k])
+                . (bs ~ Snoc as a, as ~ Init bs, a ~ Last bs)
+               => Dict (SnocList as a bs)
+defineSnocList = defineSnocList
+
+instance SnocList '[] a '[a]
+-- instance SnocList as z bs => SnocList (a ': as) z (a ': bs)
+{-# ANN consInstSnocList (ToInstance NoOverlap) #-}
+consInstSnocList :: forall (k :: Type) (as :: [k]) (z :: k) (bs :: [k]) (a :: k) (b :: k)
+                  . SnocList as z (b ': bs)
+                 => Dict (SnocList (a ': as) z (a ': b ': bs))
+consInstSnocList
+  | Dict <- unsafeEqTypes @_ @(a ': b ': bs) @(Snoc (a ': as) z)
+  , Dict <- unsafeEqTypes @_ @(a ': as) @(Init (a ': b ': bs))
+  , Dict <- unsafeEqTypes @_ @z @(Last (a ': b ': bs))
+    = defineSnocList @k @(a ': as) @z @(a ': b ': bs)
+
+-- | Represent two lists that are `Reverse` of each other
+class
+    (as ~ Reverse bs, bs ~ Reverse as, ReverseList bs as)
+      => ReverseList (as :: [k]) (bs :: [k])
+            | as -> bs, bs -> as, as -> k, bs -> k
+
+{-# ANN defineReverseList ClassDict #-}
+defineReverseList :: forall (k :: Type) (as :: [k]) (bs :: [k])
+                   . (as ~ Reverse bs, bs ~ Reverse as, ReverseList bs as)
+                  => Dict (ReverseList as bs)
+defineReverseList = defineReverseList
+
+instance ReverseList ('[] :: [k]) ('[] :: [k])
+-- instance (ReverseList as bs', SnocList bs' a (b ': bs))
+--          => ReverseList (a ': as) (b ': bs)
+{-# ANN consInstReverseList (ToInstance NoOverlap) #-}
+consInstReverseList :: forall (k :: Type)
+                              (a :: k) (as :: [k]) (b :: k) (bs :: [k])
+                              (bs' :: [k])
+                     . (ReverseList as bs', SnocList bs' a (b ': bs))
+                    => Dict (ReverseList (a ': as) (b ': bs))
+consInstReverseList
+  | Dict <- unsafeEqTypes @_ @(a ': as) @(Reverse (b ': bs))
+  , Dict <- unsafeEqTypes @_ @(b ': bs) @(Reverse (a ': as))
+    = let d1 = f d2
+          d2 = g d1
+
+          f :: Dict (ReverseList (b ': bs) (a ': as))
+            -> Dict (ReverseList (a ': as) (b ': bs))
+          f Dict = defineReverseList @k @(a ': as) @(b ': bs)
+          g :: Dict (ReverseList (a ': as) (b ': bs))
+            -> Dict (ReverseList (b ': bs) (a ': as))
+          g Dict = defineReverseList @k @(b ': bs) @(a ': as)
+      in  d1
+
+{-# ANN incohInstReverseList (ToInstance Incoherent) #-}
+incohInstReverseList :: forall (k :: Type) (as :: [k]) (bs :: [k])
+                      . bs ~ Reverse as
+                     => Dict (ReverseList as bs)
+incohInstReverseList
+  | Dict <- unsafeEqTypes @_ @as @(Reverse bs)
+    = let d1 = f d2
+          d2 = g d1
+          f :: Dict (ReverseList bs as) -> Dict (ReverseList as bs)
+          f Dict = defineReverseList @k @as @bs
+          g :: Dict (ReverseList as bs) -> Dict (ReverseList bs as)
+          g Dict = defineReverseList @k @bs @as
+      in  d1
+
+
+
+-- | Append two lists.
+type family Concat (as :: [k]) (bs :: [k]) :: [k] where
+    Concat  as       '[]       = as -- "incoherent instance"
+    Concat '[]        bs       = bs
+    Concat (a ': as)  bs       = a ': Concat as bs
+
+-- | Remove prefix @as@ from a list @asbs@ if @as@ is a prefix; fail otherwise.
+type family StripPrefix (as :: [k]) (asbs :: [k]) :: [k] where
+    StripPrefix  as        as         = '[] -- "incoherent instance"
+    StripPrefix '[]        bs         = bs
+    StripPrefix (a ': as) (a ': asbs) = StripPrefix as asbs
+
+-- | Remove suffix @bs@ from a list @asbs@ if @bs@ is a suffix; fail otherwise.
+type family StripSuffix (bs :: [k]) (asbs :: [k]) :: [k] where
+    StripSuffix '[]        as         = as -- "incoherent instance"
+    StripSuffix  bs        bs         = '[]
+    StripSuffix  bs       (a ': asbs) = a ': StripSuffix bs asbs
+
+-- | Represent a triple of lists forming a relation @(as ++ bs) ~ asbs@
+--
+--   NB: functional dependency @bs asbs -> as@ does not seem to be possible,
+--       because dependency check happens before constraints check and does not
+--       take constraints into account.
+class
+    ( asbs ~ Concat as bs
+    , as   ~ StripSuffix bs asbs
+    , bs   ~ StripPrefix as asbs
+    ) => ConcatList (as :: [k]) (bs :: [k]) (asbs :: [k])
+            | as bs -> asbs, as asbs -> bs --, bs asbs -> as
+            , as -> k, bs -> k, asbs -> k
+
+{-# ANN defineConcatList ClassDict #-}
+defineConcatList :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
+                  . ( asbs ~ Concat as bs
+                    , as   ~ StripSuffix bs asbs
+                    , bs   ~ StripPrefix as asbs
+                    )
+                 => Dict (ConcatList as bs asbs)
+defineConcatList = defineConcatList
+
+instance {-# INCOHERENT #-} ConcatList as '[] as
+instance ConcatList '[] bs bs
+-- I have to implement next instance via the deriving plugin to let GHC know
+-- that equality constraints are satisfied.
+-- instance ConcatList  as bs asbs => ConcatList (a ': as) bs (a ': asbs)
+{-# ANN consInstConcatList (ToInstance NoOverlap) #-}
+consInstConcatList :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k]) (a :: k)
+                    . ConcatList as bs asbs
+                   => Dict (ConcatList (a ': as) bs (a ': asbs))
+consInstConcatList
+  | Dict <- unsafeEqTypes @_ @(a ': as)   @(StripSuffix bs (a ': asbs))
+  , Dict <- unsafeEqTypes @_ @bs          @(StripPrefix (a ': as) (a ': asbs))
+  , Dict <- unsafeEqTypes @_ @(a ': asbs) @(Concat (a ': as) bs)
+    = defineConcatList @k @(a ': as) @bs @(a ': asbs)
+
+-- | Derive @ConcatList@ given @Concat@
+inferConcat :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
+             . asbs ~ Concat as bs
+            => Dict (ConcatList as bs asbs)
+inferConcat
+  | Dict <- unsafeEqTypes @_ @as @(StripSuffix bs asbs)
+  , Dict <- unsafeEqTypes @_ @bs @(StripPrefix as asbs)
+    = defineConcatList
+
+-- | Derive @ConcatList@ given @StripSuffix@
+inferStripSuffix :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
+                  . as ~ StripSuffix bs asbs
+                 => Dict (ConcatList as bs asbs)
+inferStripSuffix
+  | Dict <- unsafeEqTypes @_ @bs @(StripPrefix as asbs)
+  , Dict <- unsafeEqTypes @_ @asbs @(Concat as bs)
+    = defineConcatList
+
+-- | Derive @ConcatList@ given @StripPrefix@
+inferStripPrefix :: forall (k :: Type) (as :: [k]) (bs :: [k]) (asbs :: [k])
+                  . bs ~ StripPrefix as asbs
+                 => Dict (ConcatList as bs asbs)
+inferStripPrefix
+  | Dict <- unsafeEqTypes @_ @as @(StripSuffix bs asbs)
+  , Dict <- unsafeEqTypes @_ @asbs @(Concat as bs)
+    = defineConcatList
+
+unsafeEqTypes :: forall k (a :: k) (b :: k)
+               . Dict (a ~ b)
+unsafeEqTypes = unsafeCoerce (Dict :: Dict (a ~ a))
