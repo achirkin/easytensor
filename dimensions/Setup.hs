4cc9a381ad09c7a013a121cc02231f0ac2de649d
achirkin <achirkin@users.noreply.github.com>
2019-08-08 11:18:50 +0200

    Improve haddocks

--- dimensions/Setup.hs
+++ dimensions/Setup.hs
@@ -1,2 +1,32 @@
+{-
+Disable some errors and warnings during the haddock pass
+  (caused by compiler plugins and hs-boot)
+ -}
+{-# OPTIONS_GHC -Wall #-}
+module Main (main) where
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { confHook = \a -> confHook simpleUserHooks a . tweakFlags }
+
+tweakFlags :: ConfigFlags -> ConfigFlags
+tweakFlags flags = flags
+  { configProgramArgs = addHaddockArgs (configProgramArgs flags) }
+
+addHaddockArgs :: [(String, [String])] -> [(String, [String])]
+addHaddockArgs []
+  = [("haddock", newHaddockGhcArgs)]
+addHaddockArgs (("haddock", args):otherProgsArgs)
+  = ("haddock", args ++ newHaddockGhcArgs) : otherProgsArgs
+addHaddockArgs (progArgs:otherProgsArgs)
+  = progArgs : addHaddockArgs otherProgsArgs
+
+newHaddockGhcArgs :: [String]
+newHaddockGhcArgs =
+  [ "--optghc=-fdefer-type-errors"
+  , "--optghc=-fno-warn-deferred-type-errors"
+  , "--optghc=-fno-warn-missing-home-modules"
+  ]
--- dimensions/src/Numeric/Tuple.hs
+++ dimensions/src/Numeric/Tuple.hs
@@ -16,6 +16,8 @@ import qualified Numeric.Tuple.Lazy   as TL
 import           Numeric.Tuple.Strict as TS
 import           Unsafe.Coerce        (unsafeCoerce)
 
+-- | /O(n)/ Convert a lazy @Tuple@ to a strict @Tuple@, forcing all its values
+--   to the WHNF along the way.
 toStrict :: TL.Tuple xs -> TS.Tuple xs
 toStrict U = U
 toStrict (TL.Id x :* xs)
@@ -23,6 +25,7 @@ toStrict (TL.Id x :* xs)
         !ys = toStrict xs
     in y :* ys
 
+-- | /O(n)/ Convert a strict @Tuple@ to a lazy @Tuple@ by means of a simple coercion.
 fromStrict :: TS.Tuple xs -> TL.Tuple xs
 fromStrict = unsafeCoerce
 {-# INLINE fromStrict #-}
