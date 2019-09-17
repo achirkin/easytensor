ff9dc5efe0be919f662ffcb8c5f106dddbd12130
achirkin <achirkin@users.noreply.github.com>
2019-09-17 17:22:44 +0200

    A cleaner re-implementation of SubSpace

--- easytensor/test/Numeric/DataFrame/SubSpaceTest.hs
+++ easytensor/test/Numeric/DataFrame/SubSpaceTest.hs
@@ -36,10 +36,10 @@ prop_IndexCustom1 x = (z .! 1:*3:*2) == x
 
 prop_Foldlr :: DataFrame Double SSuff -> Property
 prop_Foldlr x = approxEq zmax
-    (ewfoldl (+) 10 z) (ewfoldr @_ @SPref (+) 0 z + 10)
+    (ewfoldl' (+) 10 z) (ewfoldr' @_ @SPref (+) 0 z + 10)
   where
     z = ewgen x :: DataFrame Double SFull
-    S zmax = ewfoldl @Double @SFull @'[] (max . abs) 1 z
+    S zmax = ewfoldl' @Double @SFull @'[] (max . abs) 1 z
 
 prop_Ewmap :: DataFrame Double SFull -> Bool
 prop_Ewmap x = x * 2 == ewmap @_  @_ @'[Last SFull] (*2) x
