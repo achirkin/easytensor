2993bc62ee8afb4180e1df4a387b1131da976798
achirkin <achirkin@users.noreply.github.com>
2019-09-23 08:59:37 +0200

    One hs-boot cycle less

--- easytensor/src/Numeric/DataFrame/Internal/Backend.hs
+++ easytensor/src/Numeric/DataFrame/Internal/Backend.hs
@@ -17,9 +17,32 @@
 #else
 {-# OPTIONS_GHC -fplugin Data.Constraint.Deriving #-}
 #endif
+{-
+This module contains all generic and specific instances for all backend
+(implementation) types. Its hs-boot counterpart provides only generic instances
+(that do not depend on BackendFamily).
+Import this module only at the root of the module hierarchy, where the specific
+implementation-dependent modules do not need to depend on it.
+But do not forget to import this module in the places that will be imported by
+the user (such as Numeric.DataFrame). So that the user can benefit from the
+compile-time knowledge of the specific backend implementation.
 
+By design the import chain is as follows:
+
+NDI.Backend.Family.hs-boot            NDI.Backend.Family
+         \                                   |
+    NDI.Backend.hs-boot               NDI.Backend (instances only)
+          \                                 /
+     NDI.BackendI                          /
+            \                             /
+    Numeric.DataFrame.Type               /
+                    \                   /
+                     \                 /
+                     Numeric.DataFrame
+
+ -}
 module Numeric.DataFrame.Internal.Backend
-  ( DFBackend, Backend (..), BackendFamily, KnownBackend ()
+  ( Backend (..), BackendFamily, KnownBackend ()
   , inferKnownBackend, inferPrimElem
     -- * Auto-deriving instances
   , inferEq, inferOrd
@@ -49,10 +72,7 @@ import qualified Numeric.DataFrame.Internal.Backend.Family as Impl (KnownBackend
                                                                     inferKnownBackend,
                                                                     inferPrimArray,
                                                                     inferPrimElem)
-import           Numeric.DataFrame.Internal.BackendI       (I)
 
--- | Implementation behind the DataFrame
-type DFBackend (t :: Type) (ds :: [Nat]) = Backend I t ds (BackendFamily t ds)
 
 -- | Backend resolver:
 --   Use this constraint to find any class instances defined for all DataFrame implementations,
@@ -70,8 +90,8 @@ instance Impl.KnownBackend t ds (BackendFamily t ds)
 newtype Backend (i :: Type) (t :: Type) (ds :: [Nat]) (backend :: Type)
     = Backend { _getBackend :: backend }
 type role Backend phantom phantom phantom representational
-type instance DeriveContext (Backend I t ds b) = b ~ BackendFamily t ds
--- When typechecker knows what the third parameter @b@ is, no instances overlap,
+type instance DeriveContext (Backend i t ds b) = b ~ BackendFamily t ds
+-- When typechecker knows what the fourth parameter @b@ is, no instances overlap,
 -- because @b@ is always always a concrete type.
 -- The "dynamic instances" derived via ToInstance have to be incoherent,
 -- because @BackendFamily t ds@ as a type family (before resolving it)
@@ -89,44 +109,44 @@ inferKnownBackend
       Dict -> Dict
 
 inferPrimElem
-  :: forall (t :: Type) (d :: Nat) (ds :: [Nat])
+  :: forall (t :: Type) (d :: Nat) (ds :: [Nat]) (i :: Type)
    . KnownBackend t (d ': ds)
-  => DFBackend t (d ': ds) -> Dict (PrimBytes t)
+  => Backend i t (d ': ds) (BackendFamily t (d ': ds)) -> Dict (PrimBytes t)
 inferPrimElem = Impl.inferPrimElem @t @d @ds . _getBackend
 
 
 
 inferEq
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Eq t, Impl.KnownBackend t ds b)
-  => Dict (Eq (Backend I t ds b))
+  => Dict (Eq (Backend i t ds b))
 inferEq
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferOrd
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Ord t, Impl.KnownBackend t ds b)
-  => Dict (Ord (Backend I t ds b))
+  => Dict (Ord (Backend i t ds b))
 inferOrd
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferProductOrder
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Ord t, Impl.KnownBackend t ds b)
-  => Dict (ProductOrder (Backend I t ds b))
+  => Dict (ProductOrder (Backend i t ds b))
 inferProductOrder
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferPONonTransitive
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Ord t, Impl.KnownBackend t ds b)
-  => Dict (Ord (NonTransitive.ProductOrd (Backend I t ds b)))
+  => Dict (Ord (NonTransitive.ProductOrd (Backend i t ds b)))
 inferPONonTransitive
     = mapDict (unsafeDerive NonTransitive.ProductOrd)
     . mapDict toBackend
@@ -134,9 +154,9 @@ inferPONonTransitive
     $ inferDeriveContext @t @ds @b undefined
 
 inferPOPartial
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Ord t, Impl.KnownBackend t ds b)
-  => Dict (Ord (Partial.ProductOrd (Backend I t ds b)))
+  => Dict (Ord (Partial.ProductOrd (Backend i t ds b)))
 inferPOPartial
     = mapDict (unsafeDerive Partial.ProductOrd)
     . mapDict toBackend
@@ -144,9 +164,9 @@ inferPOPartial
     $ inferDeriveContext @t @ds @b undefined
 
 inferBounded
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Bounded t, Impl.KnownBackend t ds b)
-  => Dict (Bounded (Backend I t ds b))
+  => Dict (Bounded (Backend i t ds b))
 inferBounded
     -- Here is an ugly trick:
     --  We don't have instance of @Bounded Float@ and @Bounded Double@.
@@ -166,27 +186,27 @@ inferBounded
     $ inferDeriveContext @t @ds @b undefined
 
 inferNum
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Num t, Impl.KnownBackend t ds b)
-  => Dict (Num (Backend I t ds b))
+  => Dict (Num (Backend i t ds b))
 inferNum
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferFractional
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Fractional t, Impl.KnownBackend t ds b)
-  => Dict (Fractional (Backend I t ds b))
+  => Dict (Fractional (Backend i t ds b))
 inferFractional
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferFloating
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (Floating t, Impl.KnownBackend t ds b)
-  => Dict (Floating (Backend I t ds b))
+  => Dict (Floating (Backend i t ds b))
 inferFloating
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
@@ -194,18 +214,18 @@ inferFloating
 
 
 inferPrimBytes
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (PrimBytes t, Dimensions ds, Impl.KnownBackend t ds b)
-  => Dict (PrimBytes (Backend I t ds b))
+  => Dict (PrimBytes (Backend i t ds b))
 inferPrimBytes
     = mapDict toBackend
     . mapDict (Sub (Impl.inferBackendInstance @t @ds))
     $ inferDeriveContext @t @ds @b undefined
 
 inferPrimArray
-  :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
+  :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
    . (PrimBytes t, Impl.KnownBackend t ds b)
-  => Dict (PrimArray t (Backend I t ds b))
+  => Dict (PrimArray t (Backend i t ds b))
 inferPrimArray
     = mapDict toBackend
     . mapDict (Sub (Impl.inferPrimArray @t @ds))
@@ -214,14 +234,14 @@ inferPrimArray
 
 -- This is the rule that cannot be encoded in the type system, but enforced
 -- as an invariant: Backend t ds b implies DeriveContext t ds b
-inferDeriveContext :: forall (t :: Type) (ds :: [Nat]) (b :: Type)
-                    . Backend I t ds b
-                   -> Dict (DeriveContext (Backend I t ds b))
+inferDeriveContext :: forall (t :: Type) (ds :: [Nat]) (b :: Type) (i :: Type)
+                    . Backend i t ds b
+                   -> Dict (DeriveContext (Backend i t ds b))
 inferDeriveContext _ = unsafeCoerce (Dict :: Dict (b ~ b))
 {-# INLINE inferDeriveContext #-}
 
 -- Backend is the newtype wrapper over b.
 -- It has the same represenation and I expect it to have the same instance behavior.
-toBackend :: forall c t ds b . c b :- c (Backend I t ds b)
+toBackend :: forall c t ds b i . c b :- c (Backend i t ds b)
 toBackend = unsafeDerive Backend
 {-# INLINE toBackend #-}
